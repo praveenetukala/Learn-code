021905d27f44d6518d09bd887599afc556f4de01
praveenetukala3@gmail.com <praveenetukala3@gmail.com>
2024-03-19 12:27:54 +0530

    removed

--- DataSourceRountingReadWrite/src/main/java/com/route/repo/UsersRepo.java
+++
@@ -1,9 +0,0 @@
-package com.route.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.route.dto.Users;
-
-public interface UsersRepo extends CrudRepository<Users, Integer> {
-
-}
