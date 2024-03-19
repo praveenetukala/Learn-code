55da5da0ddfac49fab2674397fc7d2705bf451a8
praveenetukala3@gmail.com <praveenetukala3@gmail.com>
2024-03-19 14:24:44 +0530

    modified

---
+++ AngularApps/TS-Excise-Web/src/app/login.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from './services/login.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  canActivate(): boolean {
+    if (!this.loginService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
+  }
+}
